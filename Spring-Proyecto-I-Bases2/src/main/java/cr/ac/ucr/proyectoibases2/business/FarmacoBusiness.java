d878ba925cd72c02c634bd516425f800b66788ef
ANDRES\deltadragon <andres.coto3539@gmail.com>
2017-09-02 08:33:25 -0600

    Se agrego la pantalla cargar archivo qu le toca a daniel hernandez NO toque braulio, tampoco el principal

---
+++ Spring-Proyecto-I-Bases2/src/main/java/cr/ac/ucr/proyectoibases2/business/FarmacoBusiness.java
@@ -0,0 +1,5 @@
+package cr.ac.ucr.proyectoibases2.business;
+
+public class FarmacoBusiness {
+
+}
---
+++ Spring-Proyecto-I-Bases2/src/main/java/cr/ac/ucr/proyectoibases2/controller/CargarArchivoController.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.proyectoibases2.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+public class CargarArchivoController {
+
+    @RequestMapping(value = {"/CargarArchivo"}, method = RequestMethod.GET)
+    public String Principal(Model model) {
+        return "CargarArchivo";
+    }
+}
