56c0de8f5bc56ec745cd68bdbf620a9128078b9e
Andres Coto <andres.coto3539@gmail.com>
2017-09-16 23:16:32 -0600

    se recolecta la query y se separa en el controler

--- Spring-Proyecto-I-Bases2/src/main/java/cr/ac/ucr/proyectoibases2/controller/PrincipalController.java
+++ Spring-Proyecto-I-Bases2/src/main/java/cr/ac/ucr/proyectoibases2/controller/PrincipalController.java
@@ -1,23 +1,46 @@
 package cr.ac.ucr.proyectoibases2.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import cr.ac.ucr.proyectoibases2.business.FarmacoBusiness;
-import cr.ac.ucr.proyectoibases2.domain.Farmaco;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PrincipalController {
-    @RequestMapping(value = {"/Principal"}, method = RequestMethod.GET)
+    @RequestMapping(value = { "/Principal" }, method = RequestMethod.GET)
     public String Principal(Model model) {
 
         return "Principal";
     }
-  // @RequestMapping(value = {"/Principal"}, method = RequestMethod.POST)
-   // public 
+
+    @RequestMapping(value = "/Principal", method = RequestMethod.POST)
+    public void recibeQuery(@RequestParam("query") String query) {
+        String[] auxSelect = query.split("select");
+        String aux = converToString(auxSelect);
+        String[] auxFrom = aux.split("from");
+        String aux1 = converToString(auxFrom);
+        String[] auxWhere = aux1.split("where");
+        String aux2 = converToString(auxWhere);
+        String[] consulta = aux2.split(",");
+        String auxConsulta = converToString(consulta);
+        String[] result = auxConsulta.split(" ");
+        mostrar(result);
+    }
+
+    public void mostrar(String[] consulta) {
+        for (String recorre : consulta) {
+            System.out.println(recorre);
+        }
+    }
+
+    public String converToString(String[] consulta) {
+        String result = "";
+        for (String s : consulta) {
+            result += s + "\n";
+        }
+        return result.trim();
+    }
+
 }
